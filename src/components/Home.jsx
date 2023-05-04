60a93c990e3a46c60ea5af3d77a4a563e992ab90
OluTayo-Dev <olusolatemitayo656@gmail.com>
2023-05-04 10:23:04 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,318 @@
+import React from 'react';
+import Log from '../asset/logo.png';
+import tem1 from '../asset/tem1.png';
+import tem2 from '../asset/tem2.png';
+import zoom from '../asset/zoom.png';
+import logo2 from '../asset/logo2.png';
+import logo3 from '../asset/logo3.png';
+import logo4 from '../asset/logo4.png';
+import logo5 from '../asset/logo5.png';
+import fav1 from '../asset/fav1.png';
+import fav2 from '../asset/fav2.png';
+import tem3 from '../asset/tem3.png';
+import fav3 from '../asset/fav3.png';
+import fav4 from '../asset/fav4.png';
+import tem5 from '../asset/tem5.png';
+import fav5 from '../asset/fav5.png';
+import fav6 from '../asset/fav6.png';
+import fav7 from '../asset/fav7.png';
+import fav8 from '../asset/fav8.png';
+import list1 from '../asset/list1.png';
+import list2 from '../asset/list2.png';
+import list3 from '../asset/list3.png';
+import list4 from '../asset/list4.png';
+import list5 from '../asset/list5.png';
+import list6 from '../asset/list6.png';
+import listfav1 from '../asset/listfav1.png';
+import listfav2 from '../asset/listfav2.png';
+import listfav3 from '../asset/listfav3.png';
+import listfav4 from '../asset/listfav4.png';
+import tem6 from '../asset/tem6.png';
+import fb from '../asset/fb.png';
+import tweet from '../asset/tweet.png';
+import insta from '../asset/insta.png';
+import youtube from '../asset/youtube.png';
+import skype from '../asset/skype.png';
+import log from '../asset/log.png';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div class="whole">
+       <div class="header">
+        
+                <img src={Log} id="logo" />
+                 <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Courses</a></li>
+                    <li><a href="#">Testimonial</a></li>
+                    <li><a href="#">Community</a></li>
+                  </ul>
+
+                    <div class="butt">
+                      <button>Enroll Now</button>
+                    </div>
+                 
+                
+
+            </div>
+      <div class="blue">
+          
+            <div class="content">
+              <option>Grow your skills</option>
+              <option>to advance your</option>
+              <option>career path</option>
+              <h5>Build Your Future With Our Quality Education. The Best And Largest All-</h5> <br />
+              <h5>In-One Online Tutoring Platform In The World</h5>
+              <div class="amend">
+                <div class="get">
+                 <button class="get">Get Started Now</button>  
+                </div>
+                <div class="now">
+                 <button>Enroll Now</button>
+                </div>
+              </div>
+              <img src={tem2} id="tem2"/>
+              <option id="option">255k+</option> <br />
+              <option id="option">Previews</option>
+            </div>
+            <div class="tem1">
+              <img src={tem1} />
+            </div>
+            
+        </div>
+        <div class="logo-container">
+            <div class="zoom">
+             <img src={zoom}></img>
+            </div>
+            <div class="zoom">
+             <img src={logo2}></img>
+            </div>
+            <div class="zoom">
+             <img src={logo3}></img>
+            </div>
+            <div class="zoom">
+             <img src={logo4}></img>
+            </div>
+            <div class="zoom">
+              <img src={logo5}></img>
+            </div>
+        
+        </div>
+        <div class="vid">
+          <div class="text">
+              <h1>High quality</h1>
+              <h1>video,audio</h1>
+              <h1>& live classes</h1>
+              <h4>High-Definition Video Is Video Of Higher Resolution And Quality Than Standard</h4>
+              <h4>Definition. While There’s No Standard Meaning For High Definition, Generally Any</h4>
+              <h4> Standard Video Image</h4>
+
+              <button id="view">View Courses</button>
+                 <div class="first">
+                   <div class="fav1"> <img src={fav1} alt=" "/>  Audio <br/> Classes</div>
+                   <div class="fav2"> <img src={fav2} alt="" /> Live  <br/> Classes</div>
+                </div>
+
+                <div class="second">
+                  <div class="fav3"> <img src={fav3} alt="" /> Recorded <br/> Classes</div>
+                  <div class="fav4"> <img src={fav4} alt="" /> 50+  <br/> Notes</div>
+                </div>
+          </div> 
+
+          
+         <div class="image">
+            <div class="image1">
+              <img src={tem2} />
+              <img src={tem3} />
+
+          </div>
+        </div>
+        
+        
+
+
+
+
+        
+
+
+      </div>
+      <div class="this">  <h1> This is why We Are <br/>Best from Others</h1></div>
+        
+
+      <div  class="why">
+        <div class="pictest">
+           <h4>High-Definition Video Is Video Of Higher Resolution And Quality Than Standard</h4>
+           <h4> Definition. While There’s No Standard Meaning For High Definition, Generally Any</h4>
+           <h4>Standard Video Image</h4>
+        
+          <div class="tem"> <img src={tem5} /></div>
+        </div>
+        <div class="card">
+          <div class="fav5">
+            <img src={fav5} /> 
+               <h6>Experienced Mentors</h6>
+               <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition.</p>
+      
+          </div>
+          <div class="fav6">
+            <img src={fav6} />
+            <h6>One-On-One Meetings</h6>
+            <p id="light">High-Definition Video of Higher Resolution And Quality Than Standard Definition. High-Definition Video of Higher Resolution And 
+            Quality Than Standard Definition</p>
+               
+          </div>
+          <div class="fav7">
+            <img src={fav7} />
+            <h6>One-On-One Meetings</h6>
+            <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Definition Video of Higher Resolution And
+            Quality than the Standard Definition.</p>
+               
+          </div>
+          <div class="fav8">
+            <img src={fav8} />
+            <h6>Affordable Prices</h6>
+            <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Definition Video of Higher Resolution And 
+             Quality than the Standard Definition.</p>
+               
+          </div>
+        
+          <div class="click">
+          <p id="font">Browse Your Popular Courses</p>
+            <p>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. While There’s No Standard Meaning For High Definition, Generally Any Standard Video Image</p>
+
+            <ul>
+              <li><a href="#">All Categories</a></li>
+              <li><a href="#">Design</a></li>
+              <li><a href="#">Development</a></li>
+              <li><a href="#">Marketing</a></li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
+      <div class="Card">
+          <div class="list1">
+            <img src={list1} />
+            <p>Introduction To User Research In Ux Design</p>
+          </div>
+          <div class="list2">
+            <img src={list2} />
+            <p>Introduction To New Marketing Audience</p>
+          </div>
+          <div class="list3">
+            <img src={list3} />
+            <p>Introduction To Html, Css & Bootstrap</p>
+          </div>
+          <div class="list4">
+            <img src={list4} />
+            <p>Introduction To Javascript, Git & GitHub</p>
+          </div>
+          <div class="list5">
+            <img src={list5} />
+            <p>Introduction To Outroom Marketing Analysis</p>
+          </div>
+          <div class="list6">
+            <img src={list6} />
+            <p>Introduction To Live Marketing Analysis</p>
+          </div>
+          
+        </div>
+
+        <div class="button">
+          <button id="last">Explore All Courses</button>
+          <h1>What Our Clients Are Saying</h1>
+          <h4>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition.</h4> <br/>
+           <h4> While There’s No Standard Meaning For High Definition, Generally Any Standard Video Image</h4>
+        </div>
+        <div class="coloros">
+          <div class="list-fav1">
+            <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World</p>
+            <img src={listfav1} /> <p>Bella Moon <br/> Product Designer </p>
+          </div>
+          <div class="list-fav2">
+            <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World</p>
+            <img src={listfav2} /> <p>Samuel Fortune <br/> Product Designer</p>
+          </div>
+          <div class="list-fav3">
+            <p id="light">High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Hbuild Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The Worldd Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World</p>
+            <img src={listfav4} /> <p>Bella Moon <br/> Product Designer</p>
+          </div>
+        </div>
+        <div class="group">
+            <h6 id="hi">Join our Community</h6>
+            <h1 id="hello">Are You Ready To Connect With The Future</h1>
+            <h1 id="hello">Talent Of The Tech World</h1>
+            <h3 id="tag">Meet Up With Other Teachers And Grow Together</h3>
+               <img src={tem6} id="join" />
+             <div class="button2">
+                <button id="ton">Join Our Community</button>
+            </div>
+            
+        </div>
+        <div class="frequently">
+          <h1>Frequently Asked Questions</h1>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition.</p>
+          <p>While There’s No Standard Meaning For High Definition, Generally Any Standard Video Image</p>
+        </div>
+        
+        <div class="plusAll">
+        
+          <div class="plus"> <h2>Is There a Free Trial Available?</h2>     <h2>+</h2></div>
+          <hr />
+          <div class="plus"> <h2>Can I Change My Plan Later?</h2>          <h2>+</h2></div>
+          <hr />
+          <div class="plus">  <h2>Are This Course Lifetime?</h2>            <h2>+</h2></div>
+          <hr />
+          <div class="plus">   <h2>Do I Get Certified After Taking This Course</h2>      <h2>+</h2></div>
+          <hr />
+          <div class="plus">    <h2>How Do I reach out to Mentors?</h2>        <h2>+</h2></div>
+          <hr />
+          <div class="plus">     <h2>Do we Get Job ready After Taking This Course?</h2>      <h2>+</h2></div>
+          <hr />
+        </div>
+        <div class="footer">
+          <div class="foot">
+            <img src={log} />
+            <p>Reach out to Us on any of our Social</p>
+             <p> Media Networks</p>
+            <img src={fb} />
+            <img src={tweet} />
+            <img src={insta} />
+            <img src={youtube} />
+            <img src={skype} />
+          </div>
+          <div class="foot">
+            <h3>Useful Links</h3>
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Our Courses</p>
+            <p>Testimonials</p>
+            <p>Our Community</p>
+
+          </div>
+          <div class="foot">
+            <h3>Community</h3>
+            <p>Help Centers</p>
+            <p>Partners</p>
+            <p>Suggestion</p>
+            <p>Blog</p>
+            <p>Newsletter</p>
+          </div>
+          <div class="footLast">
+            <h3>Subscribe Us</h3>
+            <input type="email" id="in" placeholder="nft123@gmail.com" />
+              <div class="out">
+               <button id="out">Send Message</button>
+              </div>
+          </div>
+        </div>
+
+        
+        
+    </div> 
+  )
+}
+
